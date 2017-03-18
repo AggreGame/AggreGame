9915430da8e9e266a476f1955328507865156af1
Allan Gopez <allan.gopez@icloud.com>
2017-03-18 13:55:11 -0700

    added twitch API and ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,18 +24,41 @@ $.ajax(settings).done(function (response) {
   $.ajax(settings).done(function (response) {
  	console.log(response);
  	var url = "https://images.igdb.com/igdb/image/upload/t_cover_big/" + response[0].cover.cloudinary_id
- 	
-	
+
+
 	$("#thumbnail").attr("src", url);
 	$("#panel-left, #panel-top").html($('<p>').text("Title: " +response[0].name));
 	$("#panel-top").append($('<p>').text("Rating: " + response[0].aggregated_rating));
 	$("#summary").html($('<p>').text("Summary: " + response[0].summary));
 	$("#panel-left").append($('<p>').text("Release Date: " + response[0].release_dates[0].human));
-	
+
 	$(".game").append($('<p>').text("Story: " + response[0].storyline));
 
-<a href="/platforms/win" data-reactid="44">PC (Microsoft Windows)</a>
-
- });	
+ });
 })
+
+// twitch API
+// ======================================================================
+var twitchSettings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.twitch.tv/kraken/search/streams?query=" + queryURL,
+  "method": "GET",
+  "headers": {
+    "client-id": "w5185xydst8a2ijuvc2lwnvdpoqznk",
+    "accept": "application/vnd.twitchtv.v4+json",
+  }
+}
+
+$.ajax(twitchSettings).done(function (response) {
+  console.log(response);
+  console.log(response.streams[0].preview.medium);
+
+  $("#query-query").append("<img>");
+
+});
+
+
+// DO NOT CODE BELOW THIS LINE: END OF FILE
+// ======================================================================
 });
