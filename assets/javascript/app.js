990ead53303e3e3bfaf939f70ed7fb418213a132
Abtin Ravanipour <Abtin Ravanipour>
2017-03-19 10:29:58 -0700

    organizing code and renamed the user reviews

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-var searchQuery= "horizon-zero-dawn"
+var searchQuery= "bioshock"
 
 var settings = {
   "async": true,
@@ -15,28 +15,27 @@ var settings = {
   }
 }
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-  console.log(response[0].id);
-  settings.url = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/" + response[0].id + "?fields=*"
-  console.log(settings.url)
+	$.ajax(settings).done(function (response) {
+	  console.log(response);
+	  console.log(response[0].id);
+	  settings.url = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/" + response[0].id + "?fields=*"
 
-  $.ajax(settings).done(function (response) {
- 	console.log(response);
+	  $.ajax(settings).done(function (response) {
+	 	console.log(response);
+	 	var url = "https://images.igdb.com/igdb/image/upload/t_cover_big/" + response[0].cover.cloudinary_id;
+	 	var backgroundImg = "background-image:url('https://images.igdb.com/igdb/image/upload/t_screenshot_big/" + response[0].screenshots[0].cloudinary_id + ".png')";
 
- 	var url = "https://images.igdb.com/igdb/image/upload/t_cover_big/" + response[0].cover.cloudinary_id;
- 	var backgroundImg = "background-image:url('https://images.igdb.com/igdb/image/upload/t_screenshot_big/" + response[0].screenshots[0].cloudinary_id + ".png')";
+	 	$("body").attr("style", backgroundImg);
 
- 	$("body").attr("style", backgroundImg);
-
-	$("#thumbnail").attr("src", url);
-	$("#game-title").html("<strong>" + response[0].name + "</strong>");
-	$("#game-rating-critic").text("Critic Rating: " + parseInt(response[0].aggregated_rating));
-	$("#summary").text(response[0].summary);
-	$("#release-date").text("Release Date: " + response[0].release_dates[0].human);
-	
-	$(".game").append($('<p>').text("Story: " + response[0].storyline));
-  });	
-});
+		$("#thumbnail").attr("src", url);
+		$("#game-title").html("<strong>" + response[0].name + "</strong>");
+		$("#game-rating-user").text("User Rating: " + parseInt(response[0].rating));
+		$("#game-rating-critic").text("Critic Rating: " + parseInt(response[0].aggregated_rating));
+		$("#summary").text(response[0].summary);
+		$("#release-date").text("Release Date: " + response[0].release_dates[0].human);
+		
+		$(".game").append($('<p>').text("Story: " + response[0].storyline));
+	  });	
+	});
 });
 
