71f0a186364b6529b59763ae3fa5d0a330a5b228
CodingKaiser <falkonoe@live.com>
2017-03-20 19:46:36 -0700

    Pressing enter now loads the top result into the page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	var timer;
 	
-	function searchIgdb(searchTerm) {
+	function populateSearchSuggestions(searchTerm) {
 		var settings = {
 		  "async": true,
 		  "crossDomain": true,
@@ -64,28 +64,25 @@ $(document).ready(function() {
 		console.log($("#search").val().trim());
 		// Only let user press backspace or numbers
 		clearTimeout(timer);
-		var searchTerm = $("#search").val();
-		timer = setTimeout(function() {
-			$("#search-suggestions").empty();
-			$("#search-suggestions").removeClass("hide");
-			searchIgdb(searchTerm);
-		}, 500);
-    });
-
-	// Populate the page with information upon pressing the enter key
-	// This does not work yet
-	$("#search").on("keypress", function(event) {
 		var searchTerm = $("#search").val().trim();
 		if (event.which === 13) {
-			populatePage(searchTerm);
-		};
-	});
-
-    $("#search-bar-wrapper").on("click", ".collection-item", function() {
-    	populatePage($(this).attr("id"));
+			event.preventDefault();
+			clearTimeout(timer);
+			populatePage(searchTerm, false);
+		} else {
+			timer = setTimeout(function() {
+				$("#search-suggestions").empty();
+				$("#search-suggestions").removeClass("hide");
+				populateSearchSuggestions(searchTerm);
+			}, 500);
+		}
     });
 
-    function populatePage(elementId) {
+    $("#search-bar-wrapper").on("click", ".collection-item", function() {
+    	populatePage($(this).attr("id"), true);
+    });
+
+    function populatePage(elementId, isId) {
     	var databaseSettings = {
 		  "async": true,
 		  "crossDomain": true,
@@ -99,22 +96,36 @@ $(document).ready(function() {
 		  }
 		}
 
-		$.ajax(databaseSettings).done(function (response) {
-		 	console.log(response);
-		 	var url = "https://images.igdb.com/igdb/image/upload/t_cover_big/" + response[0].cover.cloudinary_id;
-		 	var backgroundImg = "background-image:url('https://images.igdb.com/igdb/image/upload/t_screenshot_big/" + response[0].screenshots[0].cloudinary_id + ".png')";
-
-		 	$("body").attr("style", backgroundImg);
-			$("#thumbnail").attr("src", url);
-			$("#game-title").html("<strong>" + response[0].name + "</strong>");
-			$("#game-rating-user").text("User Rating: " + parseInt(response[0].rating));
-			$("#game-rating-critic").text("Critic Rating: " + parseInt(response[0].aggregated_rating));
-			$("#summary").text(response[0].summary);
-			$("#release-date").text("Release Date: " + response[0].release_dates[0].human);
-		});
+		if (isId) {
+			$.ajax(databaseSettings).done(function (response) {
+				writeQueryResultsToPage(response);
+			});
+		} else {
+			databaseSettings.url = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search=" + elementId;
+			$.ajax(databaseSettings).done(function (response) {
+		  		databaseSettings.url = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/" + response[0].id + "?fields=*"
+			  	$.ajax(databaseSettings).done(function (response) {
+			  		writeQueryResultsToPage(response);
+				});
+			});
+		}
 		$("#search-bar-wrapper").attr("class", "hide");
 		$("#main-content").removeClass("hide");
 		$("#video-content").removeClass("hide");
+    };
+
+    function writeQueryResultsToPage(response) {
+	 	console.log(response);
+	 	var url = "https://images.igdb.com/igdb/image/upload/t_cover_big/" + response[0].cover.cloudinary_id;
+	 	var backgroundImg = "background-image:url('https://images.igdb.com/igdb/image/upload/t_screenshot_big/" + response[0].screenshots[0].cloudinary_id + ".png')";
+
+	 	$("body").attr("style", backgroundImg);
+		$("#thumbnail").attr("src", url);
+		$("#game-title").html("<strong>" + response[0].name + "</strong>");
+		$("#game-rating-user").text("User Rating: " + parseInt(response[0].rating));
+		$("#game-rating-critic").text("Critic Rating: " + parseInt(response[0].aggregated_rating));
+		$("#summary").text(response[0].summary);
+		$("#release-date").text("Release Date: " + response[0].release_dates[0].human);
     };
 
 	var searchQuery= "arkham-knight"
