8e77c64278e45e33c6497c1a82600261d514c46d
CodingKaiser <falkonoe@live.com>
2017-03-21 18:30:59 -0700

    Condensed code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,24 +2,28 @@ $(document).ready(function() {
 
 	var timer;
 
+	var igdbSettings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "",
+		"method": "GET",
+		"headers": {
+			"x-mashape-key": "8c4luXnQFumshyCCQ14GeO6WyMNHp1g3smBjsnYaWNSQ3eZl0a",
+			"accept": "application/json",
+			"cache-control": "no-cache",
+			"postman-token": "d6b0037e-a737-9698-1fdc-16bb905fd022"
+		}
+	}
+
 	$("#search-link-parent").on("click", function() {
 		$("#search-bar-wrapper").removeClass("hide");
 		$("#main-content").addClass("hide");
 	});
 	
 	function populateSearchSuggestions(searchTerm) {
-		var settings = {
-		  "async": true,
-		  "crossDomain": true,
-		  "url": "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search=" + searchTerm,
-		  "method": "GET",
-		  "headers": {
-		    "x-mashape-key": "8c4luXnQFumshyCCQ14GeO6WyMNHp1g3smBjsnYaWNSQ3eZl0a",
-		    "accept": "application/json",
-		    "cache-control": "no-cache",
-		    "postman-token": "d6b0037e-a737-9698-1fdc-16bb905fd022"
-		  }
-		}
+		var settings = igdbSettings;
+		var rawUrl = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search=" + searchTerm;
+		settings.url = rawUrl.split(' ').join('+')
 		
 		$.ajax(settings).done(function (response) {
 		  for (var i = 0; i < 5; i++) {
@@ -30,6 +34,13 @@ $(document).ready(function() {
 			 	var suggestion = $("<li class='collection-item'></li>");
 			 	suggestion.text(response[0].name);
 			 	suggestion.attr("id", response[0].id);
+			 	suggestion.attr("data-summary", response[0].summary);
+			 	suggestion.attr("data-user-rating", response[0].rating);
+			 	suggestion.attr("data-critic-rating", response[0].aggregated_rating);
+			 	suggestion.attr("data-release-date", response[0].release_dates[0].human);
+			 	suggestion.attr("data-background-img", "https://images.igdb.com/igdb/image/upload/t_screenshot_big/" + response[0].screenshots[0].cloudinary_id + ".png");
+				suggestion.attr("data-thumb", "https://images.igdb.com/igdb/image/upload/t_cover_big/" + response[0].cover.cloudinary_id);
+			 	suggestion.attr("data-title", response[0].name);
 			 	$("#search-suggestions").append(suggestion);
 			 	var url = "https://images.igdb.com/igdb/image/upload/t_cover_big;/" + response[0].cover.cloudinary_id
 			 });
@@ -73,7 +84,7 @@ $(document).ready(function() {
 		if (event.which === 13) {
 			event.preventDefault();
 			clearTimeout(timer);
-			populatePage(searchTerm, false);
+			populatePageFromNewQuery(searchTerm);
 		} else {
 			timer = setTimeout(function() {
 				$("#search-suggestions").empty();
@@ -84,53 +95,49 @@ $(document).ready(function() {
     });
 
     $("#search-bar-wrapper").on("click", ".collection-item", function() {
-    	populatePage($(this).attr("id"), true);
+		populatePageFromSuggestion($(this));
     });
 
-    function populatePage(elementId, isId) {
-    	var databaseSettings = {
-		  "async": true,
-		  "crossDomain": true,
-		  "url": "https://igdbcom-internet-game-database-v1.p.mashape.com/games/" + elementId + "?fields=*",
-		  "method": "GET",
-		  "headers": {
-		    "x-mashape-key": "8c4luXnQFumshyCCQ14GeO6WyMNHp1g3smBjsnYaWNSQ3eZl0a",
-		    "accept": "application/json",
-		    "cache-control": "no-cache",
-		    "postman-token": "d6b0037e-a737-9698-1fdc-16bb905fd022"
-		  }
-		}
+    function populatePageFromNewQuery(searchTerm) {
+    	var databaseSettings = igdbSettings;
+		databaseSettings.url = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search=" + searchTerm;
+		$.ajax(databaseSettings).done(function (response) {
+	  		databaseSettings.url = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/" + response[0].id + "?fields=*"
+		  	$.ajax(databaseSettings).done(function (response) {
+			 	console.log(response);
+			 	var url = "https://images.igdb.com/igdb/image/upload/t_cover_big/" + response[0].cover.cloudinary_id;
+			 	var backgroundImg = "background-image:url('https://images.igdb.com/igdb/image/upload/t_screenshot_big/" + response[0].screenshots[0].cloudinary_id + ".png')";
 
-		if (isId) {
-			$.ajax(databaseSettings).done(function (response) {
-				writeQueryResultsToPage(response);
+			 	$("#page-bg").attr("style", backgroundImg);
+				$("#thumbnail").attr("src", url);
+				$("#game-title").html("<strong>" + response[0].name + "</strong>");
+				$("#game-rating-user").text("User Rating: " + parseInt(response[0].rating));
+				$("#game-rating-critic").text("Critic Rating: " + parseInt(response[0].aggregated_rating));
+				$("#summary").text(response[0].summary);
+				$("#release-date").text("Release Date: " + response[0].release_dates[0].human);
 			});
-		} else {
-			databaseSettings.url = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search=" + elementId;
-			$.ajax(databaseSettings).done(function (response) {
-		  		databaseSettings.url = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/" + response[0].id + "?fields=*"
-			  	$.ajax(databaseSettings).done(function (response) {
-			  		writeQueryResultsToPage(response);
-				});
-			});
-		}
-		$("#search-bar-wrapper").attr("class", "hide");
-		$("#main-content").removeClass("hide");
-		$("#video-content").removeClass("hide");
+		});
+		prepPageForContentViewing();
     };
 
-    function writeQueryResultsToPage(response) {
-	 	console.log(response);
-	 	var url = "https://images.igdb.com/igdb/image/upload/t_cover_big/" + response[0].cover.cloudinary_id;
-	 	var backgroundImg = "background-image:url('https://images.igdb.com/igdb/image/upload/t_screenshot_big/" + response[0].screenshots[0].cloudinary_id + ".png')";
+    function populatePageFromSuggestion(htmlSuggestion) {
+	 	var url = htmlSuggestion.attr("data-thumb");
+	 	var backgroundImg = "background-image:url(" + htmlSuggestion.attr("data-background-img") + ");"
 
 	 	$("#page-bg").attr("style", backgroundImg);
 		$("#thumbnail").attr("src", url);
-		$("#game-title").html("<strong>" + response[0].name + "</strong>");
-		$("#game-rating-user").text("User Rating: " + parseInt(response[0].rating));
-		$("#game-rating-critic").text("Critic Rating: " + parseInt(response[0].aggregated_rating));
-		$("#summary").text(response[0].summary);
-		$("#release-date").text("Release Date: " + response[0].release_dates[0].human);
+		$("#game-title").html("<strong>" + htmlSuggestion.attr("data-title") + "</strong>");
+		$("#game-rating-user").text("User Rating: " + parseInt(htmlSuggestion.attr("data-user-rating")));
+		$("#game-rating-critic").text("Critic Rating: " + parseInt(htmlSuggestion.attr("data-critic-rating")));
+		$("#summary").text(htmlSuggestion.attr("data-summary"));
+		$("#release-date").text("Release Date: " + htmlSuggestion.attr("data-release-date"));
+		prepPageForContentViewing();
+    };
+
+    function prepPageForContentViewing() {
+		$("#search-bar-wrapper").attr("class", "hide");
+		$("#main-content").removeClass("hide");
+		$("#video-content").removeClass("hide");
     };
 
 	var searchQuery= "arkham-knight"
